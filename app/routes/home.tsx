bb4e4cdf1dc5ab7d418dbf628b80b4daf823cbca
Michael Nield <michael.nield@chemify.io>
2025-10-09 13:24:26 +0100

    feat(home): enable FutureCellTherapiesSection in Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <DownloadSection />
       <TeamSection />
       <ContactSection />
-      {/* <FutureCellTherapiesSection /> */}
+      <FutureCellTherapiesSection />
     </>
   );
 }
