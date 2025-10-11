e1711414d82e013ad3fa1936c64653333281f77b
Michael Nield <michael.nield@chemify.io>
2025-10-11 12:40:14 +0100

    feat(partnersSection): add first_star logo and adjust partner logo heights for improved layout

--- app/components/PartnersSection.tsx
+++ app/components/PartnersSection.tsx
@@ -3,33 +3,33 @@ import AnimatedComponent, { StaggerContainer } from "./AnimatedComponent";
 
 // Import partner logos
 import fullstackedLogo from "../assets/partners/Fullstacked-Terasaki-Institute.png";
-import firststarLogo from "../assets/partners/FirstStar.png";
+import firststarLogo from "../assets/partners/first_star.png";
 import indi from "../assets/partners/IndieBiologoOutlinedLogo.png";
 import alix from "../assets/partners/Alix.png";
 import secondstarLogo from "../assets/partners/2ndF.png";
 import capeFearBiosciencesLogo from "../assets/partners/CapeFearBiocapital.png";
 
 const partners = [
-  { src: firststarLogo, alt: "First Star", height: "75px" },
-  { src: indi, alt: "INDI", height: "50.5px" },
+  { src: firststarLogo, alt: "First Star", height: "30px" },
+  { src: indi, alt: "INDI", height: "40.5px" },
   {
     src: capeFearBiosciencesLogo,
     alt: "Cape Fear Biosciences",
-    height: "50px",
+    height: "40px",
   },
-  { src: secondstarLogo, alt: "2nd Star", height: "68.75px" },
+  { src: secondstarLogo, alt: "2nd Star", height: "48.75px" },
   {
     src: fullstackedLogo,
     alt: "Fullstacked Terasaki Institute",
     height: "50.5px",
   },
-  { src: alix, alt: "Alix", height: "50px" },
+  { src: alix, alt: "Alix", height: "40px" },
 ];
 
 const PartnersSection = () => {
   return (
     <section id="partner" className="w-full py-16 bg-white">
-      <div className="w-full max-w-7xl mx-auto px-4 py-8 md:py-16 border-t border-b border-gray-200 flex flex-col items-center">
+      <div className="w-full max-w-[1300px] mx-auto px-4 py-8 md:py-16 border-t border-b border-gray-200 flex flex-col items-center">
         <AnimatedComponent animation="fadeInUp">
           <h2 className="text-center text-gray-400 text-[18px] font-medium mb-12 tracking-wide uppercase">
             OUR INVESTORS AND PARTNERS
